24aeacb75655f46f244a9a871cc81735269ba363
roki <falgon53@yahoo.co.jp>
2023-08-08 00:59:57 +0900

    use "Noto Serif JP" in roki.dev

--- src/Contexts/Field/RokiLog.hs
+++ src/Contexts/Field/RokiLog.hs
@@ -15,9 +15,9 @@ footerAdditionalComponent :: Html ()
 footerAdditionalComponent = gAdSenseFooter <> haskellJpLogo
 
 -- c.f.
--- https://fonts.google.com/share?selection.family=Sawarabi%2BGothic%7CSawarabi%2BMincho
+-- https://fonts.google.com/share?selection.family=Noto%2BSerif%2BJP%7CUbuntu%2BMono
 font :: Html ()
 font = link_ [
-    href_ "https://fonts.googleapis.com/css2?family=Sawarabi+Gothic&family=Sawarabi+Mincho&family=Ubuntu+Mono&display=swap"
+    href_ "https://fonts.googleapis.com/css2?family=Noto+Serif+JP&family=Ubuntu+Mono&display=swap"
   , rel_ "stylesheet"
   ]
