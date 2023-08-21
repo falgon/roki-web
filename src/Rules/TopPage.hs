03f4f5931e2a6f425c81679f5626c16ebf3c78ed
roki <falgon53@yahoo.co.jp>
2023-08-22 05:17:23 +0900

    use readerT

--- src/Rules/TopPage.hs
+++ src/Rules/TopPage.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Rules.TopPage (rules) where
 
+import           Control.Monad.Extra  (mconcatMapM)
 import           Control.Monad.Reader (ReaderT (..), asks)
 import           Control.Monad.Trans  (MonadTrans (..))
 import           Data.Time.Format     (formatTime)
@@ -13,6 +14,7 @@ import           Config.Blog
 import           Config.Contributions
 import           Config.TopPage
 import           Contexts             (siteCtx)
+import qualified Contexts.Blog        as CtxBlog
 import           Rules.Blog.Type
 import           Utils                (modifyExternalLinkAttr)
 import qualified Vendor.FontAwesome   as FA
@@ -22,21 +24,28 @@ lastUpdate [] = pure $ noPostsAlt topPageConfig
 lastUpdate (x:_) = formatTime defaultTimeLocale' (postDateFormat topPageConfig)
     <$> getItemUTC defaultTimeLocale' (itemIdentifier x)
 
+introDateCtx :: [Item a] -> BlogConfReader m Compiler (Context String)
+introDateCtx posts = do
+    name <- asks blogName
+    constField (name <> "-intro-date") <$> lift (lastUpdate posts)
+
+listPostsCtx :: [Item String] -> BlogConfReader m Compiler (Context String)
+listPostsCtx posts = do
+    name <- asks blogName
+    pure $ listField (name <> "-posts") (siteCtx <> defaultContext) (pure posts)
+
 mkBlogCtx :: BlogConfReader m Compiler (Context String)
 mkBlogCtx = do
     ep <- asks blogEntryPattern
     cs <- asks blogContentSnapshot
     posts <- lift $ fmap (take $ maxTitleNum topPageConfig) . recentFirst =<< loadAllSnapshots ep cs
-    name <- asks blogName
-    introDateCtx <- lift (constField (name <> "-intro-date") <$> lastUpdate posts)
-    description <- asks blogDescription
-    pure $ mconcat [
-        listField (name <> "-posts") (siteCtx <> defaultContext) (pure posts)
-      , constField "blog-title" name
-      , constField "blog-description" description
-      , introDateCtx
-      , siteCtx
-      , defaultContext
+    mconcatMapM id [
+        listPostsCtx posts
+      , CtxBlog.title
+      , CtxBlog.description'
+      , introDateCtx posts
+      , pure siteCtx
+      , pure defaultContext
       ]
 
 rules :: [BlogConfig m] -> FA.FontAwesomeIcons -> Rules ()
