56157c494c110fb010993400b35c48a8fdb1fd89
falgon <falgon53@gmail.com>
2024-07-23 08:01:22 +0900

    fix style and add badge

--- src/Rules/Resume.hs
+++ src/Rules/Resume.hs
@@ -1,15 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Rules.Resume (rules) where
 
-import           Data.List             (sortBy)
+import           Data.Functor          ((<&>))
+import           Data.List             (intercalate, sortBy)
 import           Data.Ord              (comparing)
 import           Data.String           (IsString (..))
+import           Data.Time.Calendar    (toGregorian)
+import           Data.Time.Clock       (getCurrentTime)
+import           Data.Time.LocalTime   (LocalTime (..), utcToLocalTime)
 import           Hakyll
 import           System.FilePath       (joinPath, (</>))
 import           System.FilePath.Posix (takeBaseName)
 import           Text.Pandoc.Options   (WriterOptions)
 
-import           Config                (contentsRoot, readerOptions, siteName)
+import           Config                (contentsRoot, readerOptions, siteName,
+                                        timeZoneJST)
 import           Contexts              (siteCtx)
 import           Utils                 (modifyExternalLinkAttr)
 import qualified Vendor.FontAwesome    as FA
@@ -54,11 +59,17 @@ mdRule ss pat wOpt katexRender faIcons = match pat $ compile $ do
         >>= katexRender
         >>= saveSnapshot ss
 
+getCurrentDate :: IO String
+getCurrentDate = getCurrentTime
+    <&> toGregorian . localDay . utcToLocalTime timeZoneJST
+    <&> \(y, m, d) -> intercalate "%2F" [show y, show m, show d]
+
 -- TODO:
 -- * use ReaderT monad
 -- * add last update (take from current date)
 rules :: WriterOptions -> (Item String -> Compiler (Item String)) -> FA.FontAwesomeIcons -> Rules ()
 rules wOpt katexRender faIcons = do
+    lastUpdate <- preprocess getCurrentDate
     mdRule resumeSnapshot (fromList [aboutMeIdent]) wOpt katexRender faIcons
         *> mdRule resumeSnapshot resumeCareerPattern wOpt katexRender faIcons
         *> mdRule resumeSnapshot (fromList [otherActivitiesIdent]) wOpt katexRender faIcons
@@ -68,7 +79,7 @@ rules wOpt katexRender faIcons = do
             am <- loadSnapshotBody aboutMeIdent resumeSnapshot
             career <- sortByNum <$> loadAllSnapshots resumeCareerPattern resumeSnapshot
             oc <- loadSnapshotBody otherActivitiesIdent resumeSnapshot
-            let resumeCtx' = resumeCtx am career oc
+            let resumeCtx' = resumeCtx am career oc lastUpdate
             getResourceBody
                 >>= applyAsTemplate resumeCtx'
                 >>= loadAndApplyTemplate rootTemplate resumeCtx'
@@ -77,13 +88,14 @@ rules wOpt katexRender faIcons = do
                 >>= FA.render faIcons
     where
         resumeSnapshot = "resumeSS"
-        resumeCtx am career oc = mconcat [
+        resumeCtx am career oc lastUpdate = mconcat [
             constField "title" $ "resume - " <> siteName
           , siteCtx
           , defaultContext
           , constField "about-me-body" am
           , constField "other-activities-body" oc
           , listField "career-list" careerCtx $ pure career
+          , constField "last-update" lastUpdate
           ]
         careerCtx = mconcat [
             metadataField
