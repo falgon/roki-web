319cf5bbbbc8c3325cbab2085c2e2d4793d7502b
falgon <falgon53@gmail.com>
2024-07-23 04:08:17 +0900

    generate more md

--- src/Rules/Resume.hs
+++ src/Rules/Resume.hs
@@ -3,6 +3,7 @@ module Rules.Resume (rules) where
 
 import           Data.List             (sortBy)
 import           Data.Ord              (comparing)
+import           Data.String           (IsString (..))
 import           Hakyll
 import           System.FilePath       (joinPath, (</>))
 import           System.FilePath.Posix (takeBaseName)
@@ -13,46 +14,67 @@ import           Contexts              (siteCtx)
 import           Utils                 (modifyExternalLinkAttr)
 import qualified Vendor.FontAwesome    as FA
 
+resumeRoot :: FilePath
+resumeRoot = joinPath [contentsRoot, "resume"]
+
+aboutMeIdent :: Identifier
+aboutMeIdent = fromString $ joinPath [resumeRoot, "about_me.md"]
+
 resumeCareerPattern :: Pattern
 resumeCareerPattern = fromRegex $ mconcat
     [ "(^"
-    , joinPath [contentsRoot, "resume", "career", "[0-9]+\\.md"]
+    , joinPath [resumeRoot, "career", "[0-9]+\\.md"]
     , "$)"
     ]
+
+otherActivitiesIdent :: Identifier
+otherActivitiesIdent = fromString $ joinPath [resumeRoot, "other_activities.md"]
 
 sortByNumber :: [Item a] -> [Item a]
 sortByNumber = sortBy
     $ comparing
     $ (read :: String -> Int) . takeBaseName . toFilePath . itemIdentifier
 
+mdRule :: Snapshot
+    -> Pattern
+    -> WriterOptions
+    -> (Item String -> Compiler (Item String))
+    -> FA.FontAwesomeIcons
+    -> Rules ()
+mdRule ss pat wOpt katexRender faIcons = match pat $ compile $ do
+    pandocCompilerWith readerOptions wOpt
+        >>= modifyExternalLinkAttr
+        >>= relativizeUrls
+        >>= FA.render faIcons
+        >>= katexRender
+        >>= saveSnapshot ss
+
 rules :: WriterOptions -> (Item String -> Compiler (Item String)) -> FA.FontAwesomeIcons -> Rules ()
 rules wOpt katexRender faIcons = do
-    match resumeCareerPattern $
-        compile $ do
-            pandocCompilerWith readerOptions wOpt
-                >>= modifyExternalLinkAttr
-                >>= relativizeUrls
-                >>= FA.render faIcons
-                >>= katexRender
-                >>= saveSnapshot careerSnapshot
-
+    mdRule resumeSnapshot (fromList [aboutMeIdent]) wOpt katexRender faIcons
+        *> mdRule resumeSnapshot resumeCareerPattern wOpt katexRender faIcons
+        *> mdRule resumeSnapshot (fromList [otherActivitiesIdent]) wOpt katexRender faIcons
     match resumeJPPath $ do
         route $ gsubRoute (contentsRoot </> "pages/") (const mempty)
         compile $ do
-            career <- sortByNumber <$> loadAllSnapshots resumeCareerPattern careerSnapshot
+            am <- loadSnapshotBody aboutMeIdent resumeSnapshot
+            career <- sortByNumber <$> loadAllSnapshots resumeCareerPattern resumeSnapshot
+            oc <- loadSnapshotBody otherActivitiesIdent resumeSnapshot
             if null career then error $ show resumeCareerPattern else
                 getResourceBody
-                    >>= applyAsTemplate (resumeCtx career)
-                    >>= loadAndApplyTemplate rootTemplate (resumeCtx career)
+                    >>= applyAsTemplate (resumeCtx am career oc)
+                    >>= loadAndApplyTemplate rootTemplate (resumeCtx am career oc)
                     >>= modifyExternalLinkAttr
                     >>= relativizeUrls
                     >>= FA.render faIcons
     where
-        careerSnapshot = "careerSS"
-        resumeCtx career = mconcat [
+        resumeSnapshot = "resumeSS"
+        resumeCtx am career oc = mconcat [
             constField "title" $ "resume - " <> siteName
           , siteCtx
           , defaultContext
+          , constField "about-me-body" am
+          , constField "other-activities-body" oc
           , listField "career-list" careerCtx $ pure career
           ]
         careerCtx = mconcat [
