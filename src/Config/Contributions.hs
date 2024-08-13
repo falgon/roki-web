adc8ede1f389ddc449366756fdc6589665168ba9
Roki <falgon53@yahoo.co.jp>
2024-08-13 18:42:07 +0900

    draft: add get pinned repos from github

--- src/Config/Contributions.hs
+++ src/Config/Contributions.hs
@@ -1,19 +1,30 @@
-{-# LANGUAGE BangPatterns, DeriveGeneric, OverloadedStrings #-}
+{-# LANGUAGE BangPatterns, DeriveGeneric, DerivingStrategies,
+             DuplicateRecordFields, OverloadedStrings, QuasiQuotes,
+             TemplateHaskell, TypeFamilies, LambdaCase #-}
 module Config.Contributions (
+    reqGitHubPinnedRepo,
     renderProjectsList,
-    renderContributionsTable
+    renderContributionsTable,
+    GetPinnedReposUserPinnedItemsNodes
 ) where
 
 import           Control.Monad         (forM_)
 import           Control.Monad.Fix     (fix)
+import qualified Data.ByteString.UTF8  as BU
 import           Data.Functor.Identity (Identity)
+import           Data.Morpheus.Client  (declareLocalTypesInline, fetch, raw)
 import           Data.String           (IsString (..))
-import qualified Data.Text.Lazy        as TL
+import           Data.Text.Lazy        as TL
 import           Dhall                 (FromDhall, Generic, Natural, auto,
                                         input)
 import           Lucid.Base            (HtmlT, renderText)
 import           Lucid.Html5
+import           Network.HTTP.Req
+import           Network.URI           (URI)
+import           System.Environment    (lookupEnv)
 import           System.FilePath       ((</>))
+
+import Data.Maybe (fromJust)
 
 data Date = Date { yyyy :: Natural, mm :: Natural, dd :: Natural }
     deriving (Generic, Show)
@@ -44,9 +55,49 @@ loadProjects = input auto "./contents/config/contributions/Projects.dhall"
 loadContributes :: IO [Contribute]
 loadContributes = input auto "./contents/config/contributions/Contributions.dhall"
 
+declareLocalTypesInline "./tools/github/schema.docs.graphql"
+    [raw|
+        query GetPinnedRepos($user: String!) {
+            user(login: $user) {
+                pinnedItems(types: REPOSITORY, first: 6) {
+                    nodes {
+                        ... on Repository {
+                            __typename
+                            url
+                            name
+                            description
+                            stargazerCount
+                            languages(orderBy: {field: SIZE, direction: DESC}, first: 1) {
+                                nodes {
+                                    name
+                                    color
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    |]
+
+reqGitHubPinnedRepo :: BU.ByteString -> IO [Project]
+reqGitHubPinnedRepo token = fetch jsonRes (GetPinnedReposArgs "falgon") >>= \case
+    Left x -> print x >> pure []
+    Right (GetPinnedRepos (Just x)) -> let GetPinnedReposUserPinnedItems (Just ns) = pinnedItems x in
+        print (Prelude.map fromJust ns) >> pure []
+    _ -> pure []
+    where
+        jsonRes b = runReq defaultHttpConfig $ responseBody
+            <$> req POST (https "api.github.com" /: "graphql") (ReqBodyLbs b) lbsResponse headers
+        headers = mconcat [
+            header "Content-Type" "application/json"
+          , header "User-Agent" "roki-web"
+          , oAuth2Bearer token
+          ]
+
 renderProjectsList :: IO String
 renderProjectsList = do
-    ps <- loadProjects
+    ps <- maybe loadProjects (reqGitHubPinnedRepo . BU.fromString) =<< lookupEnv "GITHUB_TOKEN"
     return $ TL.unpack $ renderText $
         dl_ $ forM_ ps $ \p -> do
             dt_ [class_ "title is-4"] $ do
@@ -64,12 +115,13 @@ renderContributionsTable = do
                 th_ $ abbr_ [title_ "Contents"] "Contents"
                 th_ $ abbr_ [title_ "Genre"] "Genre"
                 th_ $ abbr_ [title_ "Date"] "Date"
-            tbody_ $ ($ (cs, 1 :: Int)) $ fix $ \f (cs', !i) ->
-                if null cs' then return mempty :: HtmlT Identity () else let c = head cs' in do
+            tbody_ $ ($ (cs, 1 :: Int)) $ fix $ \f (cs', !i) -> case cs' of
+                [] -> return mempty :: HtmlT Identity ()
+                (c:cs'') -> do
                     tr_ $ do
                         td_ $ fromString $ show i
                         td_ $ a_ [href_ (fromString $ link c)] $ fromString $ text c
                         td_ $ div_ [class_ "tag is-success is-light"] $ fromString $ genre c
                         td_ $ let c' = date c in
                             fromString (show (yyyy c') </> show (mm c') </> show (dd c'))
-                    f (tail cs', succ i)
+                    f (cs'', succ i)
