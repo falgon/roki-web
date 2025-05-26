7dda259a69de3f809b5aa4859a128c111d5a75bb
Roki <falgon53@yahoo.co.jp>
2025-05-26 19:19:06 +0900

    to lazy

--- tools/circleci/NotifyEmail.hs
+++ tools/circleci/NotifyEmail.hs
@@ -5,50 +5,50 @@
 module Main where
 
 import           Data.String                     (IsString (..))
-import qualified Data.Text                       as T
+import qualified Data.Text.Lazy                  as LT
 import           Network.Mail.Mime               (simpleMail')
 import           Network.Mail.SMTP               (sendMailWithLoginOAuthSTARTTLS)
 import qualified Options.Applicative             as OA
 import qualified Options.Applicative.Help.Pretty as OA
 import           System.Environment              (getEnv)
 
-messageText :: T.Text -> T.Text -> T.Text
-messageText prURL artifactsURL = T.unlines [
-    T.pack "🐥 roki-web PR Artifacts 🐥\n"
-  , T.pack "・PR: " <> prURL
-  , T.pack "・Artifacts: " <> artifactsURL
+messageText :: LT.Text -> LT.Text -> LT.Text
+messageText prURL artifactsURL = LT.unlines [
+    LT.pack "🐥 roki-web PR Artifacts 🐥\n"
+  , LT.pack "・PR: " <> prURL
+  , LT.pack "・Artifacts: " <> artifactsURL
   ]
 
 data Opts = Opts
-  { optPRURL        :: T.Text
-  , optArtifactsURL :: T.Text
-  , optFromEmail    :: T.Text
-  , optToEmail      :: T.Text
+  { optPRURL        :: LT.Text
+  , optArtifactsURL :: LT.Text
+  , optFromEmail    :: LT.Text
+  , optToEmail      :: LT.Text
   }
 
-pPRURL :: OA.Parser T.Text
-pPRURL = OA.option (T.pack <$> OA.str) $ mconcat [
+pPRURL :: OA.Parser LT.Text
+pPRURL = OA.option (LT.pack <$> OA.str) $ mconcat [
     OA.long "pr-url"
   , OA.help "A Pull request URL"
   , OA.metavar "<URL>"
   ]
 
-pArtifactsURL :: OA.Parser T.Text
-pArtifactsURL = OA.option (T.pack <$> OA.str) $ mconcat [
+pArtifactsURL :: OA.Parser LT.Text
+pArtifactsURL = OA.option (LT.pack <$> OA.str) $ mconcat [
     OA.long "artifacts-url"
   , OA.help "A Circle CI Artifacts URL"
   , OA.metavar "<URL>"
   ]
 
-pFromEmail :: OA.Parser T.Text
-pFromEmail = OA.option (T.pack <$> OA.str) $ mconcat [
+pFromEmail :: OA.Parser LT.Text
+pFromEmail = OA.option (LT.pack <$> OA.str) $ mconcat [
     OA.long "from-email"
   , OA.help "Sender email address"
   , OA.metavar "<EMAIL>"
   ]
 
-pToEmail :: OA.Parser T.Text
-pToEmail = OA.option (T.pack <$> OA.str) $ mconcat [
+pToEmail :: OA.Parser LT.Text
+pToEmail = OA.option (LT.pack <$> OA.str) $ mconcat [
     OA.long "to-email"
   , OA.help "Recipient email address"
   , OA.metavar "<EMAIL>"
@@ -67,12 +67,12 @@ optsParser = OA.info (OA.helper <*> programOptions) $ mconcat [
   , OA.progDesc "A script that sends an email notification about roki-web artifacts build completion"
   ]
 
-sendEmail :: T.Text -> T.Text -> T.Text -> String -> IO ()
-sendEmail message fromEmail toEmail token = sendMailWithLoginOAuthSTARTTLS "smtp.gmail.com" (T.unpack fromEmail) token mail
+sendEmail :: LT.Text -> LT.Text -> LT.Text -> String -> IO ()
+sendEmail message fromEmail toEmail token = sendMailWithLoginOAuthSTARTTLS "smtp.gmail.com" (LT.unpack fromEmail) token mail
   where
     mail = simpleMail'
-      (fromString $ T.unpack toEmail)    -- to
-      [fromString $ T.unpack fromEmail]  -- from
+      (fromString $ LT.unpack toEmail)  -- to
+      (fromString $ LT.unpack fromEmail)  -- from
       "roki-web PR Artifacts Notification"  -- subject
       message  -- body
 
