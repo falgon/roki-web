38acb404adfa44b28427116fe6d146f692665876
Roki <falgon53@yahoo.co.jp>
2025-07-10 01:55:11 +0900

    add tag search feature 3
    enable typescript

--- src/Rules/Src/JavaScript.hs
+++ src/Rules/Src/JavaScript.hs
@@ -6,10 +6,19 @@ import           Hakyll
 import           System.FilePath (joinPath)
 
 import           Media           (compressJsCompiler)
+import           Media.TS        (compileTypeScriptCompiler)
 
 rules :: Rules ()
-rules = match jsPath $ do
-    route $ gsubRoute "contents/" $ const mempty
-    compile compressJsCompiler
+rules = do
+    -- TypeScriptファイルの処理
+    match tsPath $ do
+        route $ gsubRoute "contents/ts/" (const "js/") `composeRoutes` setExtension "js"
+        compile compileTypeScriptCompiler
+
+    -- JavaScriptファイルの処理（手動で作成されたもの）
+    match jsPath $ do
+        route $ gsubRoute "contents/" $ const mempty
+        compile compressJsCompiler
     where
+        tsPath = fromGlob $ joinPath ["contents", "ts", "**"]
         jsPath = fromGlob $ joinPath ["contents", "js", "**"]
